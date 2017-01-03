5cdd4ab605cef167920db315b7bf6236aa24b777
Alexey Nosov <nayretech@gmail.com>
2017-01-03 12:00:13 +0700

    module-4 Complete Module 4 solution

--- module4/SpeakGoodBye.js
+++ module4/SpeakGoodBye.js
@@ -1,25 +1,14 @@
-// NOTE! The steps in this file are basically identical to the ones you
-// performed in the SpeakHello.js file.
+/* jshint esversion: 6 */
 
-// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
-// See Lecture 52, part 2
+(function (context) {
+    'use strict';
 
+    let speakWord = "Good Bye";
+    let byeSpeaker = {
+        speak(name) {
+            console.log(`${speakWord} ${name}`);
+        }
+    };
 
-// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
-// the "speak" method and which you will expose to the global context
-// See Lecture 52, part 1
-// var byeSpeaker =
-
-// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
-
-// STEP 8: Rewrite the 'speak' function such that it is attached to the
-// byeSpeaker object instead of being a standalone function.
-// See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
-
-// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
-// 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+    context.byeSpeaker = byeSpeaker;
+})(window);
